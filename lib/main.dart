f92eaf294e7afad6eef388af4895ccf323902c66
alidaoud7 <deleali60@gmail.com>
2023-06-19 18:12:50 +0100

    11111111111 22222222

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Ds',
+      title: 'Flutters Ds',
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
