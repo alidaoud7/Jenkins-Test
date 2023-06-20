3a910748cb612991e65360b3ee68497f15518d8d
alidaoud7 <deleali60@gmail.com>
2023-06-20 10:00:48 +0100

    jjjj

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 's s'),
+      home: const MyHomePage(title: 'ss s'),
     );
   }
 }
